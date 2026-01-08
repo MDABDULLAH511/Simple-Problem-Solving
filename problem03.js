e76c7b19631700ce293f26412fdb9d31db6b05ed
Md Abdullah <155mdabdullah@gmail.com>
2026-01-08 15:03:06 +0600

    Solve Problem 3: Check for palindrome

---
+++ problem03.js
@@ -0,0 +1,22 @@
+// Problem 3: Check for Palindrome
+// Write a function that checks if a string is a palindrome (reads the same forward and backward).
+
+// Example:
+// Input: "madam"
+// Output: true
+// Input: "hello"
+// Output: false
+
+
+function isPalindrome(str) {
+  let reversed = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+
+  return str === reversed;
+}
+
+console.log(isPalindrome("madam")); 
+console.log(isPalindrome("hello"));
